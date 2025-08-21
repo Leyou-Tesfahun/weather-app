c5c880c7fa55106cb000fef432ab372cb5fa132c
liyou <leyoutesfahun09@gmail.com>
2025-08-21 10:45:01 +0300

    Remove unused react.svg asset from project

---
+++ src/assets/weatherImages.js
@@ -0,0 +1,18 @@
+// Import all weather images
+import sunImg from './sun.png';
+import partialSunImg from './partial sun.png';
+import cloudImg from './cloud.png';
+import rainImg from './rain.png';
+import snowImg from './snow.png';
+import windImg from './wind.png';
+
+export const weatherImages = {
+  'sun.png': sunImg,
+  'partial sun.png': partialSunImg,
+  'cloud.png': cloudImg,
+  'rain.png': rainImg,
+  'snow.png': snowImg,
+  'wind.png': windImg,
+};
+
+export default weatherImages;
